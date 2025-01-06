0eef831229acd1eb939d491c071efac06013c5e1
Mark Heimer <13_mark@protonmail.com>
2025-01-06 11:12:34 +0100

    use provided reaction center extraction

--- get_reaction_center.py
+++ get_reaction_center.py
@@ -1,6 +1,7 @@
 import networkx as nx
+from rc_extract import get_rc
 
-def get_reaction_center(its_graph):
+def get_reaction_center_old(its_graph):
     reaction_center_edges = [edge for edge in its_graph.edges.data() if edge[2]["standard_order"] != 0]
 
     reaction_center_nodes = set()
@@ -10,3 +11,6 @@ def get_reaction_center(its_graph):
         reaction_center_nodes.add(end_node)
 
     return nx.induced_subgraph(its_graph, reaction_center_nodes)
+
+def get_reaction_center(graph):
+    return get_rc(graph)
