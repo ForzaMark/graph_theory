998f4b3a591e37a3349f801cd581819d8321524f
Mark Heimer <mark.heimer@t2informatik.de>
2025-01-15 17:12:44 +0100

    wip other

--- util.py
+++ util.py
@@ -6,8 +6,8 @@ def aggregate_charge_and_element_attributes(reaction):
 
     return reaction
 
-def load_reactions():
-    data = load_from_pickle("./ITS_graphs.pkl.gz")
+def load_reactions(path = "./ITS_graphs.pkl.gz"):
+    data = load_from_pickle(path)
     reactions = [reaction["ITS"] for reaction in data]
     reactions = [aggregate_charge_and_element_attributes(reaction) for reaction in reactions]
 
